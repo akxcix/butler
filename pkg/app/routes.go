05a4427da5e27fdfb44e93935476181d11c7b874
Adarsh Kumar <contactadarshk@gmail.com>
2023-08-14 09:46:59 +0530

    add cors

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -6,12 +6,23 @@ import (
 	"github.com/akxcix/butler/pkg/services/waitlist"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func createRoutes(waitlistService *waitlist.Service) *chi.Mux {
 	r := chi.NewRouter()
 
 	// global middlewares
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(handlers.LogRequest)
 
 	// general routes
