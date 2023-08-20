be247e5d02f51f5cd1ddf6faa5c3d8857c3c505d
Adarsh Kumar <contactadarshk@gmail.com>
2023-08-20 20:21:00 +0530

    add daemon run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # this is not the right way, but its a temp solution
 docker build -t butler:latest .
-docker run -p 8080:8080 -v $(pwd)/config.yml:/app/config.yml butler:latest
+docker run -d -p 8080:8080 -v $(pwd)/config.yml:/app/config.yml butler:latest
